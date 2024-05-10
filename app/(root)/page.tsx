9b0ef258084b402d5d37695f9272b117078ad27a
Raoera Romino <Romixeno@gmail.com>
2024-05-10 13:15:08 +0300

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,39 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Romino",
+    lastName: "Raoera",
+    email: "Romixeno@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 125.65 }, { currentBalance: 300.65 }]}
+      ></RightSidebar>
+    </section>
+  );
 };
 
-export default Home; 
+export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,11 @@ const ibmPlexSerif = IBM_Plex_Serif({
 });
 
 export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
     icon: "/icons/logo.svg",
   },
-  title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone",
 };
 
 export default function RootLayout({
